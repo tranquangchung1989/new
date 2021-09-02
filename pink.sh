7ce50b1a10b78fd40f74e40b8993fd4e85818ca0
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-02 20:25:48 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=127.0.0.1:2020
-WALLET=0x86fc2bf54eeb39059638c91ade97322948882b9c
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy2
+WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy
 
 cd "$(dirname "$0")"
 
