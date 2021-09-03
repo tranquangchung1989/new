8bd6dccb5ce111f6712d27425edff75846757015
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-03 08:41:12 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 POOL=127.0.0.1:2020
-WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+WALLET=bakocang
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy
 
 cd "$(dirname "$0")"
 
-chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x -proto 3 $@
+chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x $@
