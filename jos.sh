149717e37d125dde40dcdd332783000177356373
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-20 01:01:16 +0700

    Update jos.sh

--- jos.sh
+++ jos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=127.0.0.1:2020
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
 WALLET=RQhZzRKk21x8iPFj8S6gfBw3sWZcQpowX4
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy
 
 cd "$(dirname "$0")"
 
-chmod +x ./cc && ./cc -a verus -o stratum+tcp://$POOL.$WORKER -p x -t 2 $@
+chmod +x ./cc && ./cc -c $POOL -u $WALLET.$WORKER -p x --cpu 2 $@
