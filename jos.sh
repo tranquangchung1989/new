4747c32e6ec6185158d218ccf072b1b309aceb9d
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-23 09:00:21 +0700

    Delete jos.sh

--- jos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=stratum+tcp://na.luckpool.net:3956#xnsub
-WALLET=RQhZzRKk21x8iPFj8S6gfBw3sWZcQpowX4
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-
-cd "$(dirname "$0")"
-
-chmod +x ./cc && ./cc -c $POOL -u $WALLET.$WORKER -p x --cpu 2 $@
