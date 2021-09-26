fabe212f6a869e0dfdd0c7e6ed08fb5d1756b424
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-26 13:25:49 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=daggerhashimoto.usa-west.nicehash.com:3353
-WALLET=35aYSrPF8mveijs3ZUzEBE3w3mAxHfok5M
+POOL=ethash.poolbinance.com:1800
+WALLET=bakocang
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -dpool $POOL -pass x -proto 4 -stales 0 $@
+chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x -stales 0 $@
