cbc50df501d20ca49a79112af13036c62c3223af
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-09-27 13:22:13 +0800

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=127.0.0.1:2020
-WALLET=bakocang
-WORKER=$(echo "$(cat /proc/sys/kernel/hostname)" | tr . _ )
+POOL=eth.2miners.com:2020
+WALLET=0xf7273ad5d0e81c6395ba1aa880c57fb99de9414a
+WORKER=$(echo "$(cat /proc/sys/kernel/hostname)" | tr . _ )-TQC
 
 cd "$(dirname "$0")"
 
